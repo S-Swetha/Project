b5002f865b2dc9a0a1287e2d7bd4163766c51c1b
Swetha <swethaselva5597@gmail.com>
2017-11-18 20:43:30 +0530

    My project

---
+++ project-backend/src/main/java/niit/project_backend/App.java
@@ -0,0 +1,13 @@
+package niit.project_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
